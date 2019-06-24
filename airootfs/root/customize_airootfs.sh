88b95d3d0917abc5a848d8c0ab7095b5e0fc7631
damoklov <mishanya@protonmail.com>
2019-06-24 17:25:28 +0000

    Deleted bluetooth service entry

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -22,6 +22,5 @@ sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service NetworkManager.service
-systemctl enable bluetooth.service
 systemctl set-default graphical.target
 
