fc49d14c9c043f2b70d32bc95778a723d231446f
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-06-25 15:28:41 +0300

    Delete autoconfig.sh

--- airootfs/etc/skel/Desktop/Scripts/autoconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash  
-echo -e "#\n# ~/.bash_profile\n#\n\n\n[[ -f ~/.bashrc ]] && . ~/.bashrc\n\n\n[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bash_profile
-echo -e "#!/bin/sh\n#\n# ~/.xinitrc\n#\n\n# Executed by startx (run your window manager from here)\n\n\nif [ -d /etc/X11/xinit/xinitrc.d ]; then\n  for f in /etc/X11/xinit/xinitrc.d/*; do\n    [ -x "$f" ] && . "$f"\n  done\n  unset f\nfi\n\nexec cinnamon-session" >> ~/.xinitrc
-sudo pacman -Syyu --noconfirm
-cd ~/
-git clone https://aur.archlinux.org/yay.git
-cd yay/
-makepkg -si
-rm -f ~/Desktop/Scripts/autoconfig.sh
