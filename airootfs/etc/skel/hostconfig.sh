f52e6a81cc8ba8810a3ddaecfccc4a3fcfb51525
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-06-25 11:58:15 +0300

    Update hostconfig.sh

--- airootfs/etc/skel/hostconfig.sh
+++ airootfs/etc/skel/hostconfig.sh
@@ -3,4 +3,5 @@ sudo post-clean
 sudo pacman -Rns virtualbox-guest-utils --noconfirm
 sudo pacman -Rns virtualbox-guest-dkms --noconfirm
 sudo rm /etc/modules-load.d/virtualbox.conf
-sudo rm -f ~/hostconfig.sh ~/Desktop/hostconfig.desktop
+sudo rm -f ~/Desktop/hostconfig.desktop
+sudo rm -f ~/hostconfig.sh
