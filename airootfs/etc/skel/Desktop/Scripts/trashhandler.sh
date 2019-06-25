92c1edc42fea50a57f86bb99166a7fe55c18e4e1
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-06-25 15:13:19 +0300

    Create trashhandler.sh

---
+++ airootfs/etc/skel/Desktop/Scripts/trashhandler.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -f /usr/share/applications/bssh.desktop /usr/share/applications/bvnc.desktop /usr/share/applications/avahi-discover.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop
+rm -f ~/Desktop/installArch.desktop
+rm -f ~/Desktop/Scripts/trashhandler.sh
