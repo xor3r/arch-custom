dfe1dbe292b925ac932a414125566003572de2a8
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-06-25 16:10:39 +0300

    Delete hostconfig.sh

--- airootfs/etc/skel/Desktop/Scripts/hostconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo post-clean
-sudo pacman -Rns virtualbox-guest-utils --noconfirm
-sudo pacman -Rns virtualbox-guest-dkms --noconfirm
-sudo rm /etc/modules-load.d/virtualbox.conf
-sudo rm -f ~/Desktop/Scripts/hostconfig.sh
