546034abfa59d878969b91102b468db64a5641cd
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-06-25 11:45:31 +0300

    Update and rename airootfs/etc/skel/Desktop/hostconfig.sh to airootfs/etc/skel/hostconfig.sh

--- airootfs/etc/skel/hostconfig.sh
+++ airootfs/etc/skel/hostconfig.sh
@@ -3,5 +3,4 @@ sudo post-clean
 sudo pacman -Rns virtualbox-guest-utils --noconfirm
 sudo pacman -Rns virtualbox-guest-dkms --noconfirm
 sudo rm /etc/modules-load.d/virtualbox.conf
-cd ~/Desktop
-sudo rm -f hostconfig.sh hostconfig.desktop
+sudo rm -f ~/hostconfig.sh ~/Desktop/hostconfig.desktop
